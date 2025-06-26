898c1d48e582f99b336c32cffc901ea9d24a1b85
Pedro Moretti <pfontanamoretti@gmail.com>
2025-06-26 11:27:43 +0100

    Adding Laucher
    Added full launcher functionalities

---
+++ MeadGamesLauncher/MeadGamesLauncher/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MeadGamesLauncher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
